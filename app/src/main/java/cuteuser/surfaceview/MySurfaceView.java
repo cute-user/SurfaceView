e7b91b8ca6a8c60459243b879f76b68da1dbfc5e
fixer256 <yudin.rs@yandex.ru>
2022-01-24 18:16:29 +0300

    upd

--- app/src/main/java/cuteuser/surfaceview/MySurfaceView.java
+++ app/src/main/java/cuteuser/surfaceview/MySurfaceView.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
+
 
 public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
     public MySurfaceView(Context context) {
@@ -16,24 +18,35 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     }
 
     Thread drawThread;
+    float x,y,r;
+    boolean started = false;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = event.getX();
+        y = event.getY();
+        r = 0;
+        started = true;
+        return false;
+    }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
         drawThread = new Thread() {
-            float x = 0;
             Paint paint = new Paint();
 
             @Override
             public void run() {
-                paint.setColor(Color.WHITE);
+                paint.setColor(Color.YELLOW);
                 while (true) {
                     Canvas canvas = getHolder().lockCanvas();
-                    canvas.drawColor(Color.BLACK);
-                    canvas.drawCircle(0, x, 50, paint);
+                    canvas.drawColor(Color.BLUE);
+                    if (started) canvas.drawCircle(x, y, r, paint);
                     getHolder().unlockCanvasAndPost(canvas);
-                    x++;
+                    r+=5;
                     try {
-                        Thread.sleep(1500);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
